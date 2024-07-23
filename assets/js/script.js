ae01980b1eb0af0c1a1948cec8929910e42d5346
Pablo Moreno <ehtomer@gmail.com>
2024-07-23 17:43:11 +0200

    update time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-let btnTriste = document.getElementById("btnTriste");
-let btnNeutre = document.getElementById("btnNeutre");
-let btnContent = document.getElementById("btnContent");
-
 // MORPHING FUNCTION
 function svgMorphing(target, from, to, duration) {
   let timeline = anime({
@@ -20,19 +16,22 @@ function svgMorphing(target, from, to, duration) {
 var actualExpression = "-";
 var durationTransition = 0;
 var durationPose = 0;
+var durationDefault = 400;
 
 const galleryFaces = document.querySelectorAll(".galleryFace");
 
 for (let i = 0; i < galleryFaces.length; i++) {
   galleryFaces[i].addEventListener("click", function () {
     actualExpression = galleryFaces[i].getAttribute("data-expresion");
-    selectFace();
+    document.getElementById("actualExpressionTop").innerHTML = actualExpression;
+    document.getElementById("actualExpression").innerHTML = actualExpression;
     chooseExpression();
-    updateRecapNumbers();
+    putExpressionOnPreview();
+    updateRecap();
   });
 }
 
-function selectFace() {
+function chooseExpression() {
   galleryFacesAll = document.querySelectorAll(".galleryFace");
   for (let i = 0; i < galleryFacesAll.length; i++) {
     galleryFacesAll[i].classList.remove("selectedFace");
@@ -42,28 +41,153 @@ function selectFace() {
     .classList.add("selectedFace");
 }
 
-function chooseExpression() {
-  if (actualExpression == "joie") {
-    document.getElementById("actualExpressionTop").innerHTML = "Joie";
+function putExpressionOnPreview() {
+  var visageExpression = "visage" + actualExpression;
+  var levresExpression = "levres" + actualExpression;
+  var palaisExpression = "palais" + actualExpression;
+  var dentsExpression = "dents" + actualExpression;
+  var sourcilGaucheExpression = "sourcilGauche" + actualExpression;
+  var sourcilDroitExpression = "sourcilDroit" + actualExpression;
+  console.log(visageExpression);
 
-    document.getElementById("actualExpression").innerHTML = "Joie";
+  svgMorphing(
+    visage,
+    svgPaths["visageNeutre"],
+    svgPaths[visageExpression],
+    durationDefault
+  );
 
-    svgMorphing(visage, visageContent, visageContent);
-    svgMorphing(levres, levresContent, levresContent);
-    svgMorphing(palais, palaisContent, palaisContent);
-    svgMorphing(dents, dentsContent, dentsContent);
-    svgMorphing(sourcilGauche, sourcilGaucheContent, sourcilGaucheContent);
-    svgMorphing(sourcilDroit, sourcilDroitContent, sourcilDroitContent);
-  }
+  svgMorphing(
+    levres,
+    svgPaths["levresNeutre"],
+    svgPaths[levresExpression],
+    durationDefault
+  );
+  svgMorphing(
+    palais,
+    svgPaths["palaisNeutre"],
+    svgPaths[palaisExpression],
+    durationDefault
+  );
+  svgMorphing(
+    dents,
+    svgPaths["dentsNeutre"],
+    svgPaths[dentsExpression],
+    durationDefault
+  );
+  svgMorphing(
+    sourcilGauche,
+    svgPaths["sourcilGaucheNeutre"],
+    svgPaths[sourcilGaucheExpression],
+    durationDefault
+  );
+  svgMorphing(
+    sourcilDroit,
+    svgPaths["sourcilDroitNeutre"],
+    svgPaths[sourcilDroitExpression],
+    durationDefault
+  );
 }
 
 function playAnimation() {
-  console.log("play");
+  var visageExpression = "visage" + actualExpression;
+  var levresExpression = "levres" + actualExpression;
+  var palaisExpression = "palais" + actualExpression;
+  var dentsExpression = "dents" + actualExpression;
+  var sourcilGaucheExpression = "sourcilGauche" + actualExpression;
+  var sourcilDroitExpression = "sourcilDroit" + actualExpression;
+
+  var durationTransition = parseInt(
+    document.getElementById("durationTransition").value
+  );
+  var durationPose = parseInt(document.getElementById("durationPose").value);
+  console.log("Transition: " + durationTransition);
+  console.log("Pose: " + durationPose);
+
+  console.log("playing");
+
+  svgMorphing(
+    visage,
+    svgPaths["visageNeutre"],
+    svgPaths[visageExpression],
+    durationTransition
+  );
+  svgMorphing(
+    levres,
+    svgPaths["levresNeutre"],
+    svgPaths[levresExpression],
+    durationTransition
+  );
+  svgMorphing(
+    palais,
+    svgPaths["palaisNeutre"],
+    svgPaths[palaisExpression],
+    durationTransition
+  );
+  svgMorphing(
+    dents,
+    svgPaths["dentsNeutre"],
+    svgPaths[dentsExpression],
+    durationTransition
+  );
+  svgMorphing(
+    sourcilGauche,
+    svgPaths["sourcilGaucheNeutre"],
+    svgPaths[sourcilGaucheExpression],
+    durationTransition
+  );
+  svgMorphing(
+    sourcilDroit,
+    svgPaths["sourcilDroitNeutre"],
+    svgPaths[sourcilDroitExpression],
+    durationTransition
+  );
+  setTimeout(() => {
+    console.log("changin");
+
+    svgMorphing(
+      visage,
+      svgPaths[visageExpression],
+      svgPaths["visageNeutre"],
+      durationTransition
+    );
+
+    svgMorphing(
+      levres,
+      svgPaths[levresExpression],
+      svgPaths["levresNeutre"],
+      durationTransition
+    );
+    svgMorphing(
+      palais,
+      svgPaths[palaisExpression],
+      svgPaths["palaisNeutre"],
+      durationTransition
+    );
+    svgMorphing(
+      dents,
+      svgPaths[dentsExpression],
+      svgPaths["dentsNeutre"],
+      durationTransition
+    );
+    svgMorphing(
+      sourcilGauche,
+      svgPaths[sourcilGaucheExpression],
+      svgPaths["sourcilGaucheNeutre"],
+      durationTransition
+    );
+    svgMorphing(
+      sourcilDroit,
+      svgPaths[sourcilDroitExpression],
+      svgPaths["sourcilDroitNeutre"],
+      durationTransition
+    );
+  }, durationTransition + durationPose);
 }
 
 document.getElementById("playBtn").addEventListener("click", playAnimation);
 
-function updateRecapNumbers() {
+function updateRecap() {
   var durationTransition = parseInt(
     document.getElementById("durationTransition").value
   );
